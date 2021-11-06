bc6bd2ee5a8c2ee40d98a629113092fb1361fa09
Alexandra Antonova <antonova_sasha@list.ru>
2021-11-06 23:29:17 +0300

    homework 3

---
+++ client_for_metrics/server.py
@@ -0,0 +1,94 @@
+﻿# реализация сервера для тестирования метода get по заданию - Клиент для отправки метрик
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+
+class Server:
+    def __init__(self, host, port):
+        assert(host != ""), "empty host"
+        assert(port > 1023 and port <= 65535), "port should be integer between 1024 and 65535, got " + str(port)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((host, port))
+        self.sock.listen(1)
+        self.database = {}
+
+    def __del__(self):
+        self.sock.close()
+
+    def run(self):
+        while True:
+            conn, addr = self.sock.accept()
+            print('Соединение установлено:', addr)
+            while True:
+                # предполагаем, что запросы у нас короткие и будут всегда целиком влезать в 1024 байта
+                data = conn.recv(1024)
+                if not data:
+                    break
+                request = data.decode('utf-8')
+                print(f'Получен запрос: {ascii(request)}')
+                response = self.process_request(request)
+                print('Отправлен ответ ', response)
+                conn.sendall(response.encode("utf-8"))
+            conn.close()
+   
+    def process_request(self, request):
+        #put palm.cpu 23.7 1150864247\n
+        #get palm.cpu\n
+        #get *\n
+        parts = request.strip().split()
+        if parts[0] == 'put':
+            if len(parts) != 4:
+                return "error\nwrong command\n\n"
+            metric, percentage, timestamp = parts[1:]
+            try:
+                percentage = float(percentage)
+                timestamp = int(timestamp)
+            except:
+                return "error\nwrong command\n\n"
+            else:
+                return self.put(metric, percentage, timestamp)
+        else:
+            if parts[0] != 'get':
+                return "error\nwrong command\n\n"
+            if len(parts) != 2:
+                return "error\nwrong command\n\n"
+            if parts[1] == '*':
+                return self.get_all()
+            else:
+                return self.get(metric)
+
+    def put(self, metric, percentage, timestamp):
+        self.database.setdefault(metric, dict())
+        self.database[metric][timestamp] = percentage
+        return "ok\n\n"
+
+    def get_all(self):
+        strs = []
+        for metric in self.database:
+            for timestamp in self.database[metric]:
+                percentage = self.database[metric][timestamp]
+                strs.append(metric + " " + str(percentage) + " " + str(timestamp))
+        if len(strs) > 0:
+            return "ok\n" + "\n".join(strs) + "\n\n"
+        else:
+            return "ok\n\n"
+
+    def get(self, metric):
+        strs = []
+        for timestamp in self.database[metric]:
+            percentage = self.database[metric][timestamp]
+            strs.append(metric + " " + str(percentage) + " " + str(timestamp))
+        if len(strs) > 0:
+            return "ok\n" + "\n".join(strs) + "\n\n"
+        else:
+            return "ok\n\n"
+
+# переменная response хранит строку возвращаемую сервером, если вам для
+# тестирования клиента необходим другой ответ, измените ее
+#response = b'ok\npalm.cpu 10.5 1501864247\neardrum.cpu 15.3 1501864259\n\n'
+
+
+if __name__ == "__main__":
+    server = Server("127.0.0.1", 8888)
+    server.run()
---
+++ client_for_metrics/solution.py
@@ -0,0 +1,79 @@
+import socket
+import time
+
+class ClientError(Exception):
+    """Класс для генерации ошибок работы протокола"""
+    pass
+
+class Client:
+    def __init__(self, host, port, timeout=None):
+        assert(host != ""), "empty host"
+        assert(port > 1023 and port <= 65535), "port should be integer between 1024 and 65535, got " + str(port)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(timeout)
+        self.sock.connect((host, port))
+
+    def __del__(self):
+        self.sock.close()
+
+    def get(self, key):
+        request = "get " + key + "\n"
+        self.sock.send(request.encode("utf-8"))
+        result = b''
+        while True:
+            data = self.sock.recv(1024)
+            if data:
+                result += data
+            else:
+                break
+            if data.endswith(b"\n\n"): #ответ прочитан полностью
+                break
+        result = result.decode("utf-8")
+        js = {}
+        parts = result.strip().split("\n")
+        for p in parts[1:]:
+            metric, percentage, timestamp = p.split(" ")
+            percentage = float(percentage)
+            timestamp = int(timestamp)
+            js.setdefault(metric, list())
+            js[metric].append((int(timestamp), float(percentage)))
+        
+        #сортируем списки в каждой метрике по возрастанию timestamp
+        for metric in js:
+            js[metric] = sorted(js[metric], key=lambda t: t[0])
+        return js
+    
+    def put(self, metric, percentage, timestamp=None):
+        if timestamp is None:
+            timestamp = int(time.time())
+        request = "put " + metric + " " + str(percentage) + " " + str(timestamp) + "\n"
+        self.sock.send(request.encode("utf-8"))
+        result = b''
+        while True:
+            data = self.sock.recv(1024)
+            print("received ", data)
+            if data:
+                result += data
+            else:
+                break
+            if data.endswith(b"\n\n"): #ответ прочитан полностью
+                break
+        result = result.decode("utf-8")  
+        if result == "ok\n\n":
+            return
+        else:
+            raise ClientError
+
+
+if __name__ == "__main__":
+    client = Client("127.0.0.1", 8888, timeout=15)
+    client.put("palm.cpu", 0.5, timestamp=1150864247)
+    client.put("palm.cpu", 2.0, timestamp=1150864248)
+    client.put("palm.cpu", 0.5, timestamp=1150864248)
+    client.put("eardrum.cpu", 3, timestamp=1150864250)
+    client.put("eardrum.cpu", 4, timestamp=1150864251)
+    client.put("eardrum.memory", 4200000)
+    print(client.get("*"))
+
+
+    
